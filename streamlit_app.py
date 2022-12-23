a27cb567be2b88b4876f400f3857efb31ac4d3a0
Zeeshan Mehdi <121309621+c0ldskin@users.noreply.github.com>
2022-12-23 21:43:55 +0400

    Update streamlit_app.py
    fixed error " to '

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header( 'Breakfast Favorites")
+streamlit.header( 'Breakfast Favorites')
 
 streamlit.text(' Omega 3 & Blueberry Oatmeal') 
 streamlit.text ('03 Kale, Spinach & Rocket Smoothie')
