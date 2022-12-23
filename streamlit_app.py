790775cc381897d723515205c3758b5f45b249ce
Zeeshan Mehdi <121309621+c0ldskin@users.noreply.github.com>
2022-12-23 22:21:23 +0400

    Update streamlit_app.py
    _ casued error

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ streamlit.dataframe (my_fruit_list)
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my fruit list.set index('Fruit')
+my_fruit_list = my_fruit_list.set index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #display the table on the page
-streamlit.dataframe (my fruit list)
+streamlit.dataframe (my_fruit_list)
