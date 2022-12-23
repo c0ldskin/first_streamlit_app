4b85f5e524e07f7e35707687c51317feea5996a0
Zeeshan Mehdi <121309621+c0ldskin@users.noreply.github.com>
2022-12-23 21:44:39 +0400

    Update streamlit_app.py
    smoothies added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header( 'Breakfast Favorites')
 
-streamlit.text(' Omega 3 & Blueberry Oatmeal') 
+streamlit.text('Omega 3 & Blueberry Oatmeal') 
 streamlit.text ('03 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('4 Hard-Boiled Free-Range Egg')
 streamlit.text ('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
