d6f0c03d42e40fbddc013f0108ece8c8efbeda79
Zeeshan Mehdi <121309621+c0ldskin@users.noreply.github.com>
2022-12-23 21:42:47 +0400

    Update streamlit_app.py
    updated breakfast header and items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text ('Omega 3 & Blueberry Oatmeal') 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+
+streamlit.header( 'Breakfast Favorites")
+
+streamlit.text(' Omega 3 & Blueberry Oatmeal') 
+streamlit.text ('03 Kale, Spinach & Rocket Smoothie")
+streamlit.text ('4 Hard-Boiled Free-Range Egg')
+streamlit.text ('Avocado Toast')
